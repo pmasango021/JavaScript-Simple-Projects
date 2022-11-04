0846dec16951aed6462729b296e9f01ecf319fb1
pmasango021 <103104826+pmasango021@users.noreply.github.com>
2022-11-04 08:17:14 +0200

    TypeShapes.js

---
+++ MyAge-DogAge.js
@@ -0,0 +1,11 @@
+let aged = true;
+let realAge = 0;
+
+if (aged) {
+    // realAge = '4 years';
+    realAge = 4;
+}
+
+let dogAge = realAge * 7;
+
+console.log(`${dogAge} years`);
---
+++ TypeShapes.js
@@ -0,0 +1,11 @@
+// let firstName = 'muriel!'.toUpperCase();
+// // firstName.toUppercase();
+// console.log(firstName);
+//
+// firstName = firstName.length;
+// // console.log(firstName.lenght);
+
+let firstName = 'muriel!'.toUpperCase();
+console.log(firstName);
+firstName = firstName.length
+console.log(firstName);
